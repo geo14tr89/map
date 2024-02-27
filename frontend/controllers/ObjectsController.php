c72f3d619398b68f4937b308dc523d822fedaf7c
andrii.kharkovyi <andrikkg@ukr.net>
2024-02-27 16:43:01 +0200

    create filters for objects

--- frontend/controllers/ObjectsController.php
+++ frontend/controllers/ObjectsController.php
@@ -7,11 +7,10 @@ use common\models\ObjectItem;
 use common\models\Objects;
 use frontend\components\FrontendController;
 use frontend\models\ObjectResponse;
+use frontend\models\ObjectsForm;
 use frontend\models\SingleObjectResponse;
-use SplQueue;
 use Throwable;
 use Yii;
-use yii\authclient\AuthAction;
 use yii\base\InvalidConfigException;
 use yii\db\Exception;
 use yii\db\StaleObjectException;
@@ -28,12 +27,16 @@ class ObjectsController extends FrontendController
         Yii::$app->response->format = Response::FORMAT_JSON;
 
         $params = Yii::$app->request->getQueryParams();
-        $north = $params['north'];
-        $south = $params['south'];
-        $west = $params['west'];
-        $east = $params['east'];
 
-        return Objects::getObjectsByPolygon($north, $south, $west, $east);
+        $model = new ObjectsForm();
+        if ($model->load($params, '') && $model->validate()) {
+            $query = Objects::getObjectsByPolygon($model->north, $model->south, $model->west, $model->east);
+            $query = $model->filter($query);
+
+            return $query->asArray()->all();
+        }
+
+        return $model->getErrors();
     }
 
     public function actionView(int $id): SingleObjectResponse
@@ -129,168 +132,8 @@ class ObjectsController extends FrontendController
     public function actionTest()
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
-        $array = [2, 4, 10, 6];
 
-        $a = 'hello';
-        $b = 'world';
-        list($a, $b) = [$b, $a];
-        //return $this->finHighest($array);
-
-        $array1 = [33, 15, 10, 2, 7, 34, 67, 89, 32, 78, 29];
-        //return $this->quickSort($array1);
-
-        $this->search('you');
-
-        //$this->selectionSort($array);
-
-//        $array = [];
-//        for ($i = 0; $i <= 100; $i++) {
-//            $array[] = $i;
-//        }
-//
-//        $low = 0;
-//        $high = count($array) - 1;
-//
-//        while ($low <= $high) {
-//            $middle = ($low + $high) / 2;
-//            $guess = $array[$middle];
-//            if ($guess === 54) {
-//                return $middle;
-//            }
-//
-//            if ($guess > 54) {
-//                $high = $middle - 1;
-//            } else {
-//                $low = $middle + 1;
-//            }
-//        }
-//        return null;
-
-        return $array;
-        $array = [
-            [
-                'name' => 'Andrii',
-                'age' => 29,
-                'status' => 'active',
-                'email' => 'andrikkg@ukr.net'
-            ],
-            [
-                'name' => 'Andrii123',
-                'age' => 29,
-                'status' => 'active',
-                'email' => 'andrikkg@ukr.net'
-            ],
-            [
-                'name' => 'Andrii456',
-                'age' => 29,
-                'status' => 'active',
-                'email' => 'andrikkg@ukr.net'
-            ],
-        ];
-
-        $neededKeys = ['name', 'age'];
-
-        $result = [];
-
-        foreach ($array as $values) {
-            $result[] = array_filter($values, function ($value, $key) use ($neededKeys) {
-                if (in_array($key, $neededKeys, true)) {
-                    return $value;
-                }
-                return '';
-            }, ARRAY_FILTER_USE_BOTH);
-        }
-
-        return $result;
-    }
-
-    public function findSmallest(array $array)
-    {
-        $smallest = $array[0];
-        $smallEstIndex = 0;
-        $count = count($array);
-        for ($i = 0; $i < $count; $i++) {
-            if (isset($array[$i]) && $array[$i] < $smallest) {
-                $smallest = $array[$i];
-                $smallEstIndex = $i;
-            }
-        }
-        return $smallEstIndex;
-    }
-
-    public function sum(array $array)
-    {
-        if (empty($array)) {
-            return 0;
-        }
-
-        $first = $array[0];
-        unset($array[0]);
-        $array = array_values($array);
-
-        return $first + $this->sum($array);
-    }
-
-    public function finHighest(array $array)
-    {
-        sort($array);
-        $result = array_reverse($array);
-
-        return $result[0];
-    }
-
-    public function quickSort(array $array)
-    {
-        $count = count($array);
-        if ($count <= 1) {
-            return $array;
-        }
-
-        $supportKey = array_rand($array);
-
-        $lessThenSupport = [];
-        $moreThenSupport = [];
-
-//        for ($i = 1; $i < $count; $i++) {
-//            if ($array[$i] < $supportElement) {
-//                $lessThenSupport[] = $array[$i];
-//            } else {
-//                $moreThenSupport[] = $array[$i];
-//            }
-//        }
-
-        foreach ($array as $item) {
-            if ($item === $array[$supportKey]) {
-                continue;
-            }
-            if ($item < $array[$supportKey]) {
-                $lessThenSupport[] = $item;
-            } else {
-                $moreThenSupport[] = $item;
-            }
-        }
-
-        return array_merge($this->quickSort($lessThenSupport), [$array[$supportKey]], $this->quickSort($moreThenSupport));
-    }
-
-    public function search(string $name)
-    {
-        $searchQueue = new SplQueue();
-        $searchQueue->enqueue($name);
-        $searched = [];
-
-        while ($searchQueue) {
-            $person = $searchQueue->pop();
-            if (in_array($person, $searched, true) === false) {
-                if ($person === 'Mango') {
-                    return true;
-                }
-
-                $searchQueue->enqueue($person);
-                $searched[] = $person;
-            }
-        }
-        return false;
+        return Yii::$app->db->dsn;
     }
 
     /**
---
+++ frontend/models/ObjectsForm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace frontend\models;
+
+use yii\base\Model;
+use yii\db\ActiveQuery;
+
+class ObjectsForm extends Model
+{
+    public string $north;
+    public string $south;
+    public string $west;
+    public string $east;
+    public ?string $building_type = null;
+    public ?string $architect = null;
+    public ?string $architectural_style = null;
+
+    /**
+     * @return string[]
+     */
+    public function rules(): array
+    {
+        return [
+            ['north', 'required'],
+            ['south', 'required'],
+            ['west', 'required'],
+            ['east', 'required'],
+            ['building_type', 'safe'],
+            ['architect', 'safe'],
+            ['architectural_style', 'safe'],
+        ];
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @return ActiveQuery
+     */
+    public function filter(ActiveQuery $query): ActiveQuery
+    {
+        if ($this->building_type !== null) {
+            $buildingTypeArray = explode(',', $this->building_type);
+            if (count($buildingTypeArray) > 1) {
+                foreach ($buildingTypeArray as $type) {
+                    $query->orWhere("JSON_EXTRACT(custom_fields, '$.building_type') = '{$type}'");
+                }
+            } else {
+                $query->andWhere("JSON_EXTRACT(custom_fields, '$.building_type') = '{$buildingTypeArray[0]}'");
+            }
+        }
+        if ($this->architect !== null) {
+            $query->andWhere("JSON_EXTRACT(custom_fields, '$.architect') = '{$this->architect}'");
+        }
+        if ($this->architectural_style !== null) {
+            $query->andWhere("JSON_EXTRACT(custom_fields, '$.architectural_style') = '{$this->architectural_style}'");
+        }
+
+        return $query;
+    }
+}
